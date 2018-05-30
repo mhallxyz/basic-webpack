784c684c57a33227e2343488547893d2d5a578ac
Mathew Hall <m@mhall.xyz>
2018-05-31 00:47:18 +0100

    Image appears

--- build/bundle.js
+++ build/bundle.js
@@ -63,11 +63,24 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var image = document.createElement('img');
+image.src = 'http://mhall.xyz/static/media/mhallxyz1.92974727.png';
+image.height = 400;
+
+document.body.appendChild(image);
+
+/***/ },
+/* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -82,19 +95,21 @@ var sum = function sum(a, b) {
 exports.default = sum;
 
 /***/ },
-/* 1 */
+/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _sum = __webpack_require__(0);
+var _sum = __webpack_require__(1);
 
 var _sum2 = _interopRequireDefault(_sum);
 
+__webpack_require__(0);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var total = (0, _sum2.default)(10, 5);
+var total = (0, _sum2.default)(10, 12);
 console.log(total);
 
 /***/ }
---
+++ src/image_viewer.js
@@ -0,0 +1,5 @@
+const image = document.createElement('img');
+image.src = 'http://mhall.xyz/static/media/mhallxyz1.92974727.png';
+image.height = 400;
+
+document.body.appendChild(image);
